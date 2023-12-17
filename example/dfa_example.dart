f79f7dbd90efa6d52ad30d87ea8f74f0298932cb
Mohammad Yaghubkhani <Mhwmdyaghubkhani@gmail.com>
2023-12-17 18:39:45 +0330

    Added DFA minimization example

--- example/dfa_example.dart
+++ example/dfa_example.dart
@@ -1,3 +1,5 @@
+import 'package:automata_simulator/src/dfa_minimization.dart';
+
 import 'dfa_collection.dart';
 import 'utils.dart';
 
@@ -10,6 +12,25 @@ void main() {
       final List<String> testCases = ['451.2351', '145.', '12ah'];
 
       final reports = testCases.map((testCase) => generateDFAReportForInput(dfa, testCase));
+
+      final completeDFAWithoutUnreachableStates = dfa.toComplete(
+        (states) {
+          return genStringSinkState(states, 'SinkState');
+        },
+      ).withoutUnreachableStates();
+
+      minimizeDFA(completeDFAWithoutUnreachableStates).fold(
+        (error) => print('Error: $error'),
+        (minimizedDFA) {
+          print('DFA:');
+          print(prettyDFA(dfa));
+          print('');
+          print('Minimized DFA:');
+
+          print(prettyDFA(minimizedDFA));
+          print('');
+        },
+      );
 
       reports.forEach(print);
     },
