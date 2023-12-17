23cd435b60132b51730409d11132673a6a40d024
Mohammad Yaghubkhani <Mhwmdyaghubkhani@gmail.com>
2023-12-17 18:39:11 +0330

    New utils for pretty print DFAs and methods for generating unique FAStates with String and int type

--- example/utils.dart
+++ example/utils.dart
@@ -1,4 +1,4 @@
-import 'package:automata_simulator/src/dfa.dart';
+import 'package:automata_simulator/automata_simulator.dart';
 
 String generateDFAReportForInput(DFA dfa, String inputString) {
   final StringBuffer stringBuffer = StringBuffer();
@@ -16,4 +16,27 @@ String generateDFAReportForInput(DFA dfa, String inputString) {
   stringBuffer.writeln('Accepting States are: ${dfa.acceptingStates.map((e) => e.name).join(', ')}');
 
   return stringBuffer.toString();
+}
+
+String prettyDFA(DFA dfa) {
+  final String transitionFn = dfa.transitions.entries
+      .map((entry) => '&(${entry.key.$1.name}, ${entry.key.$2}) = ${entry.value.name}')
+      .join('\n');
+
+  final String machine =
+      'M({${dfa.states.map((e) => e.name).join(', ')}}, ${dfa.alphabet}, &, ${dfa.initialState.name}, {${dfa.acceptingStates.map((e) => e.name).join(', ')}})';
+
+  return '$machine\n$transitionFn';
+}
+
+FAState<String> genStringSinkState(Set<FAState<String>> states, String name) {
+  if (!states.contains(FAState(name))) return FAState(name);
+
+  return genStringSinkState(states, '_$name');
+}
+
+FAState<int> genIntegerSinkState(Set<FAState<int>> states, int n) {
+  if (!states.contains(FAState(n))) return FAState(n);
+
+  return genIntegerSinkState(states, n + 1);
 }
