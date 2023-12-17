b4a60a00ee4ca5708b83e6a5fc85fe35765cc4d4
Mohammad Yaghubkhani <Mhwmdyaghubkhani@gmail.com>
2023-12-17 21:22:39 +0330

    Method for converting NFA to DFA

--- lib/src/nfa.dart
+++ lib/src/nfa.dart
@@ -1,6 +1,7 @@
 import 'package:collection/collection.dart';
 import 'package:fpdart/fpdart.dart';
 
+import 'dfa.dart';
 import 'fa_state.dart';
 import 'fa_validator.dart';
 import 'utils.dart';
@@ -41,6 +42,49 @@ class NFA<StateType> {
     });
   }
 
+  Either<String, DFA<String>> toDFA() {
+    final Set<String> dfaAlphabet = alphabet;
+    final FAStateSet<StateType> dfaInitialState = EquatableSet(epsilonClosure({initialState}));
+
+    Set<FAStateSet<StateType>> dfaStates = {};
+    List<({FAStateSet<StateType> from, String symbol, FAStateSet<StateType> to})> dfaSubsetConstructionTable = [];
+    Set<FAStateSet<StateType>> unprocessedDFAStates = {dfaInitialState};
+
+    while (unprocessedDFAStates.isNotEmpty) {
+      final FAStateSet<StateType> currentDFAState = unprocessedDFAStates.first;
+
+      final List<({FAStateSet<StateType> from, String symbol, FAStateSet<StateType> to})> transitionsInitialState = [];
+      final dfaTransitionsForAlphabet = dfaAlphabet.fold(transitionsInitialState, (transitions, symbol) {
+        final transition = (
+          from: currentDFAState,
+          symbol: symbol,
+          to: EquatableSet(epsilonClosure(move(currentDFAState, symbol))),
+        );
+
+        return [...transitions, transition];
+      });
+
+      final newUnprocessedDFAStates = dfaTransitionsForAlphabet.map((_) => _.to).whereNot((_) {
+        return _ == currentDFAState || dfaStates.contains(_);
+      });
+
+      dfaStates = {...dfaStates, currentDFAState};
+      unprocessedDFAStates = {...unprocessedDFAStates.skip(1), ...newUnprocessedDFAStates};
+      dfaSubsetConstructionTable = [...dfaSubsetConstructionTable, ...dfaTransitionsForAlphabet];
+    }
+
+    final dfaFinalStates = dfaStates.where((state) => state.any(finalStates.contains)).toSet();
+
+    return _createDFA(
+      dfaStates,
+      dfaAlphabet,
+      dfaSubsetConstructionTable,
+      dfaInitialState,
+      dfaFinalStates,
+      _convertToState,
+    );
+  }
+
   bool isAccepted(String input) {
     final initialValue = epsilonClosure({initialState});
 
@@ -59,6 +103,50 @@ class NFA<StateType> {
     return states.flatMap((state) => _move(state, symbol)).toSet();
   }
 
+  Option<Set<FAState<StateType>>> transitionFunction(FAState<StateType> state, Option<String> symbol) {
+    return transitions.extract<Set<FAState<StateType>>>((state, symbol));
+  }
+
+  Either<String, DFA<T>> _createDFA<T>(
+    Set<FAStateSet<StateType>> states,
+    Set<String> alphabet,
+    List<({FAStateSet<StateType> from, String symbol, FAStateSet<StateType> to})> subsetConstructionTable,
+    FAStateSet<StateType> initialState,
+    Set<FAStateSet<StateType>> finalStates,
+    FAState<T> Function(int index, FAStateSet<StateType> states) namingFunction,
+  ) {
+    final dfaWithCorrespondingNFAStates = Map<FAState<T>, FAStateSet<StateType>>.fromEntries(
+      states.where((_) => _.isNotEmpty).mapIndexed(
+        (index, stateSet) {
+          return MapEntry(namingFunction(index, stateSet), stateSet);
+        },
+      ),
+    );
+
+    FAState<T> getStateFromCorrespondingNFAStates(FAStateSet<StateType> nfaStates) {
+      return dfaWithCorrespondingNFAStates.entries.singleWhere((_) => nfaStates == _.value).key;
+    }
+
+    final namedDFAStates = states.where((_) => _.isNotEmpty).map(getStateFromCorrespondingNFAStates).toSet();
+    final namedInitialState = getStateFromCorrespondingNFAStates(initialState);
+    final namedDFAFinalStates = finalStates.where((_) => _.isNotEmpty).map(getStateFromCorrespondingNFAStates).toSet();
+
+    final namedTransitionFunction = Map.fromEntries(
+      subsetConstructionTable.where((_) => _.from.isNotEmpty && _.to.isNotEmpty).map((_) {
+        return MapEntry(
+          (getStateFromCorrespondingNFAStates(_.from), _.symbol),
+          getStateFromCorrespondingNFAStates(_.to),
+        );
+      }),
+    );
+
+    return DFA.createDFA(namedDFAStates, alphabet, namedTransitionFunction, namedInitialState, namedDFAFinalStates);
+  }
+
+  FAState<String> _convertToState(int index, FAStateSet<StateType> states) {
+    return FAState(states.map((__) => __.name).join('_'));
+  }
+
   Set<FAState<StateType>> _epsilonClosure(FAState<StateType> state, Set<FAState<StateType>> visitedStates) {
     final Set<FAState<StateType>> newStates = _reachableStates(state, None())
         .whereNot(visitedStates.contains)
@@ -74,10 +162,6 @@ class NFA<StateType> {
 
   Set<FAState<StateType>> _reachableStates(FAState<StateType> state, Option<String> symbol) {
     return transitionFunction(state, symbol).getOrElse(() => {});
-  }
-
-  Option<Set<FAState<StateType>>> transitionFunction(FAState<StateType> state, Option<String> symbol) {
-    return transitions.extract<Set<FAState<StateType>>>((state, symbol));
   }
 
   final Set<FAState<StateType>> states;
