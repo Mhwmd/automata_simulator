155d7911f3ca64ca029c24e7c59ce258b2951e72
Mohammad Yaghubkhani <Mhwmdyaghubkhani@gmail.com>
2023-12-24 17:20:05 +0330

    More readable and functional way of generating dot graph from NFA and DFA
    1- Fixed bug when there is no non-accepting or accepting states
    2 -Fixed bug of collecting symbols for currentState and nextState of NFA

--- lib/src/fa_dot_graph.dart
+++ lib/src/fa_dot_graph.dart
@@ -1,19 +1,19 @@
+import 'package:automata_simulator/src/utils.dart';
 import 'package:fpdart/fpdart.dart';
 
 import 'dfa.dart';
 import 'fa_state.dart';
 import 'nfa.dart';
-import 'utils.dart';
+
+typedef _TransitionFunction<StateType, SymbolType> = Map<(FAState<StateType>, FAState<StateType>), Set<SymbolType>>;
 
 class FADotGraph {
   FADotGraph._();
 
   static String exportDFA<StateType>(DFA<StateType> dfa) {
+    final transitionsWithCollectedSymbols = _collectDFASymbolsWithSameTransition(dfa.transitions);
+
     final nonAcceptingStates = dfa.states.difference(dfa.acceptingStates);
-    final Map<(FAState<StateType>, FAState<StateType>), Set<String>> transitionSymbols = {};
-    dfa.transitions.forEach((key, nextState) {
-      transitionSymbols.update((key.$1, nextState), (symbols) => {...symbols, key.$2}, ifAbsent: () => {key.$2});
-    });
 
     final StringBuffer stringBuffer = StringBuffer();
     stringBuffer.writeln('digraph DFA {');
@@ -25,26 +25,30 @@ class FADotGraph {
     stringBuffer.writeln(_indent(1, _startNode('_start_')));
     stringBuffer.writeln('');
 
-    stringBuffer.writeln(_indent(1, _nonAcceptingNode(nonAcceptingStates.map((e) => e.name.toString()).join(' '))));
-    stringBuffer.writeln(_indent(1, _acceptingNode(dfa.acceptingStates.map((e) => e.name.toString()).join(' '))));
+    if (nonAcceptingStates.isNotEmpty) {
+      stringBuffer.writeln(_indent(1, _nonAcceptingNode(nonAcceptingStates.map((e) => e.name.toString()).join(' '))));
+    }
+    if (dfa.acceptingStates.isNotEmpty) {
+      stringBuffer.writeln(_indent(1, _acceptingNode(dfa.acceptingStates.map((e) => e.name.toString()).join(' '))));
+    }
     stringBuffer.writeln('');
 
     stringBuffer.writeln(_indent(1, _transitionNode('_start_', dfa.initialState.name.toString(), 'Start')));
     stringBuffer.writeln('');
 
-    stringBuffer.writeAll(
-      transitionSymbols.entries.map((e) {
-        final transition = (from: e.key.$1, to: e.key.$2, symbols: e.value);
+    transitionsWithCollectedSymbols.forEach((transitionNodes, symbols) {
+      final (firstNode, secondNode) = transitionNodes;
+      final edgeLabel = symbols.join(', ');
 
-        return _transitionNode(
-          transition.from.name.toString(),
-          transition.to.name.toString(),
-          transition.symbols.join(', '),
-        );
-      }).map((e) => _indent(1, e)),
-      '\n',
-    );
-    stringBuffer.writeln('');
+      stringBuffer.writeln(_indent(
+        1,
+        _transitionNode(
+          firstNode.name.toString(),
+          secondNode.name.toString(),
+          edgeLabel,
+        ),
+      ));
+    });
 
     stringBuffer.writeln('}');
 
@@ -52,15 +56,9 @@ class FADotGraph {
   }
 
   static String exportNFA<StateType>(NFA<StateType> nfa) {
+    final transitionsWithCollectedSymbols = _collectNFASymbolsWithSameTransition(nfa.transitions);
+
     final nonAcceptingStates = nfa.states.difference(nfa.acceptingStates);
-    final Map<(FAState<StateType>, EquatableSet<FAState<StateType>>), Set<Option<String>>> transitionSymbols = {};
-    nfa.transitions.forEach((key, nextStates) {
-      transitionSymbols.update(
-        (key.$1, EquatableSet(nextStates)),
-        (symbols) => {...symbols, key.$2},
-        ifAbsent: () => {key.$2},
-      );
-    });
 
     final StringBuffer stringBuffer = StringBuffer();
     stringBuffer.writeln('digraph NFA {');
@@ -72,31 +70,76 @@ class FADotGraph {
     stringBuffer.writeln(_indent(1, _startNode('_start_')));
     stringBuffer.writeln('');
 
-    stringBuffer.writeln(_indent(1, _nonAcceptingNode(nonAcceptingStates.map((e) => e.name.toString()).join(' '))));
-    stringBuffer.writeln(_indent(1, _acceptingNode(nfa.acceptingStates.map((e) => e.name.toString()).join(' '))));
+    if (nonAcceptingStates.isNotEmpty) {
+      stringBuffer.writeln(_indent(1, _nonAcceptingNode(nonAcceptingStates.map((e) => e.name.toString()).join(' '))));
+    }
+    if (nfa.acceptingStates.isNotEmpty) {
+      stringBuffer.writeln(_indent(1, _acceptingNode(nfa.acceptingStates.map((e) => e.name.toString()).join(' '))));
+    }
     stringBuffer.writeln('');
 
     stringBuffer.writeln(_indent(1, _transitionNode('_start_', nfa.initialState.name.toString(), 'Start')));
     stringBuffer.writeln('');
 
-    transitionSymbols.forEach((key, symbolsEdge) {
-      final transitionsNode = key.$2.map((nextState) {
-        return _indent(
-          1,
-          _transitionNode(
-            key.$1.name.toString(),
-            nextState.name.toString(),
-            symbolsEdge.map((e) => e.getOrElse(() => 'ε')).join(', '),
-          ),
-        );
-      });
+    transitionsWithCollectedSymbols.forEach((transitionNodes, symbols) {
+      final (firstNode, secondNode) = transitionNodes;
+      final edgeLabel = symbols.map((e) => e.getOrElse(() => 'ε')).join(', ');
 
-      stringBuffer.writeAll(transitionsNode, '\n');
+      stringBuffer.writeln(_indent(
+        1,
+        _transitionNode(
+          firstNode.name.toString(),
+          secondNode.name.toString(),
+          edgeLabel,
+        ),
+      ));
     });
 
     stringBuffer.writeln('}');
 
     return stringBuffer.toString();
+  }
+
+  static _TransitionFunction<StateType, String> _collectDFASymbolsWithSameTransition<StateType>(
+    DFATransitionFn<StateType> transitions,
+  ) {
+    return transitions.entries.fold({}, (res, transitionFnEntry) {
+      final transitionFn = (
+        currentState: transitionFnEntry.key.$1,
+        symbol: transitionFnEntry.key.$2,
+        nextState: transitionFnEntry.value,
+      );
+
+      return res.immutableUpdate(
+        (transitionFn.currentState, transitionFn.nextState),
+        (symbols) => {...symbols, transitionFn.symbol},
+        ifAbsent: () => {transitionFn.symbol},
+      );
+    });
+  }
+
+  static _TransitionFunction<StateType, Option<String>> _collectNFASymbolsWithSameTransition<StateType>(
+    NFATransitionFn<StateType> transitions,
+  ) {
+    final flattenTransitions = transitions.entries.flatMap((transitionFnEntry) {
+      final transitionFn = (
+        currentState: transitionFnEntry.key.$1,
+        symbol: transitionFnEntry.key.$2,
+        nextStates: transitionFnEntry.value,
+      );
+
+      return transitionFn.nextStates.map((nextState) {
+        return (currentState: transitionFn.currentState, nextState: nextState, symbol: transitionFn.symbol);
+      });
+    });
+
+    return flattenTransitions.fold({}, (res, transitionFn) {
+      return res.immutableUpdate(
+        (transitionFn.currentState, transitionFn.nextState),
+        (symbols) => {...symbols, transitionFn.symbol},
+        ifAbsent: () => {transitionFn.symbol},
+      );
+    });
   }
 
   static String _indent(int n, String str) => '\t' * n + str;
